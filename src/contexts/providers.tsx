8038f046318c18e370d1bdff7880fb099b051d18
Russell Oakham <russell.oakham@and.digital>
2024-05-21 23:07:14 +0100

    Clerk - added publishable key to provider

--- src/contexts/providers.tsx
+++ src/contexts/providers.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from 'next-themes'
 
 import { Toaster } from '@/components/ui/sonner'
+import { env } from '@/env.mjs'
 
 interface ProviderProps {
 	readonly children: React.ReactNode
@@ -13,7 +14,9 @@ export function Providers({ children }: ProviderProps) {
 	return (
 		<>
 			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-				<ClerkProvider>{children}</ClerkProvider>
+				<ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+					{children}
+				</ClerkProvider>
 			</ThemeProvider>
 			<Toaster visibleToasts={1} richColors closeButton />
 		</>
