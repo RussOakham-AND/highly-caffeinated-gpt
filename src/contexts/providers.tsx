cd1ffbcd1490624d043216aefec1530b0ceb6c5d
Russell Oakham <russell.oakham@and.digital>
2024-05-21 23:19:28 +0100

    ClerkPRovider removed publishable key

--- src/contexts/providers.tsx
+++ src/contexts/providers.tsx
@@ -4,7 +4,6 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from 'next-themes'
 
 import { Toaster } from '@/components/ui/sonner'
-import { env } from '@/env.mjs'
 
 interface ProviderProps {
 	readonly children: React.ReactNode
@@ -14,9 +13,7 @@ export function Providers({ children }: ProviderProps) {
 	return (
 		<>
 			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-				<ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-					{children}
-				</ClerkProvider>
+				<ClerkProvider>{children}</ClerkProvider>
 			</ThemeProvider>
 			<Toaster visibleToasts={1} richColors closeButton />
 		</>
