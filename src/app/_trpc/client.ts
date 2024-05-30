a6db6b082bc3a6469a4b41a5eb8ff2576915c907
Russell Oakham <russell.oakham@and.digital>
2024-05-30 16:00:50 +0100

    TRPC - set max duration limit

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -2,4 +2,9 @@ import { createTRPCReact } from '@trpc/react-query'
 
 import { AppRouter } from '@/trpc'
 
+// This function can run for a maximum of 5 seconds
+export const config = {
+	maxDuration: 60,
+}
+
 export const trpc = createTRPCReact<AppRouter>({})
