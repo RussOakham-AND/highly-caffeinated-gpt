fca781936e974eac2f84032a59b6fcc26a168146
RussOakham-AND <108665426+RussOakham-AND@users.noreply.github.com>
2024-05-30 16:23:17 +0100

    TRPC - set max duration limit (#13)
    * TRPC - set max duration limit
    
    * refactor due to deprecation

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,9 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 
 import { appRouter } from '@/trpc'
 
+// This function can run for a maximum of 5 seconds
+export const maxDuration = 60
+
 const handler = (req: Request) =>
 	fetchRequestHandler({
 		endpoint: '/api/trpc',
